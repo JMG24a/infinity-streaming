554b240d1e891d8474e8de656e04716c72189825
JMG24a <jmg24a@gmail.com>
2022-07-03 10:14:09 -0400

    preparando nodos para el dom dinamico

---
+++ src/js/nodos.js
@@ -0,0 +1,28 @@
+function $(id){
+  return document.getElementById(id)
+}
+
+//home header
+const $header = $('header')
+const $headerArrow = $('header-arrow')
+const $headerSearchTile = $('header-search-title')
+const $searchForm = $('searchForm')
+
+//home trending
+const $title = $('title')
+const $trendingPreview = $('trendingPreview')
+const $trendingArticlePreview = $('trendingArticlePreview')
+const $categoriesPreview = $('categoriesPreview')
+const $btnTrending = $('btnTrending')
+
+//search
+const $headerSearch = $('header-search')
+
+//home categories
+const $categoryPreview = $('categoryPreview')
+
+//movie detail
+const $movieDetail = $('movie-detail')
+
+//generic list
+const $genericList = $('generic-list')
